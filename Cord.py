ac91a1eb3a9b685db3ee284492c827bc1414fbb4
amit-samuel-34 <42984040+amit-samuel-34@users.noreply.github.com>
2019-01-17 03:11:13 -0600

    Add files via upload

---
+++ Cord.py
@@ -0,0 +1,72 @@
+
+
+f_shrimp = (28, 325)
+f_rice = (105, 337)
+f_nori = (35, 372)
+f_roe = (93, 391)
+f_salmon = (34, 435)
+f_unagi = (101, 432)
+
+#------------------------
+
+phone = (582, 360)
+
+menu_toppings = (553, 267)
+
+t_shrimp = (490, 223)
+t_nori = (565, 221)
+t_roe = (493, 276)
+t_salmon = (570, 274)
+t_unagi = (492, 348)
+t_exit = (595, 325)
+
+menu_rice = (549, 292)
+buy_rice = (550, 285)
+
+delivery_norm = (497, 292)
+
+"""
+
+Plate cords:
+
+84 210
+None
+195 210
+None
+293 215
+None
+387 219
+None
+492 213
+None
+594 207
+None
+"""
+
+"""
+
+RGB Data
+
+im = screenGrab()
+im.getpixel(Cord.t_nori)
+(94, 49, 8)
+im.getpixel(Cord.t_roe)
+(33, 30, 11)
+im.getpixel(Cord.t_salmon)
+(127, 61, 0)
+im.getpixel(Cord.t_shrimp)
+(127, 127, 127)
+im.getpixel(Cord.t_unagi)
+(0, 0, 0)
+im.getpixel(Cord.t_unagi)
+(0, 0, 0)
+im.getpixel(Cord.buy_rice)
+(109, 123, 127)
+im.getpixel(Cord.buy_rice)
+(109, 123, 127)
+it = screenGrab()
+it.getpixel(Cord.buy_rice)
+(127, 127, 127)
+
+"""
+
